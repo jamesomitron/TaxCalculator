e5b8509216e7e27dcec432b526f8093f85ace130
James Omitiran <james.omitiran@outlook.com>
2023-12-27 01:31:14 +0100

    Add integration tests for Repository

---
+++ TaxCalculationTest/Infrastucture.Tests/DBContextExtension/ApplicationDBContextExtension.cs
@@ -0,0 +1,71 @@
+﻿
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaxCalculationTest.Infrastucture.Tests.DBContextExtension;
+
+public static class ApplicationDBContextExtension
+{
+    public static ApplicationDBContext AddTestData(this ApplicationDBContext context)
+    {
+        var postalCodes = new List<TaxPostalCode>()
+        {
+            new TaxPostalCode{ Code = "7441", TaxCalculationType = TaxType.Progressive},
+            new TaxPostalCode{ Code = "A100", TaxCalculationType = TaxType.FlatValue},
+            new TaxPostalCode{ Code = "7000", TaxCalculationType = TaxType.FlatRate},
+            new TaxPostalCode{ Code = "1000", TaxCalculationType = TaxType.Progressive}
+        };
+        _ = context.SaveChanges();
+
+        var progressiveMatrix = new List<TaxProgressiveRates>()
+        {
+            new TaxProgressiveRates{ Rate = 10, FromValue = 0, ToValue = 8350},
+            new TaxProgressiveRates{ Rate = 15, FromValue = 8351, ToValue = 33950},
+            new TaxProgressiveRates{ Rate = 25, FromValue = 33951, ToValue = 82250},
+            new TaxProgressiveRates{ Rate = 28, FromValue = 82251, ToValue = 171550},
+            new TaxProgressiveRates{ Rate = 33, FromValue = 171551, ToValue = 372950},
+            new TaxProgressiveRates{ Rate = 35, FromValue = 372951, ToValue = int.MaxValue},
+        };
+
+        context.TaxProgressiveRates.AddRange(progressiveMatrix);
+
+        
+
+        context.TaxPostalCode.AddRange(postalCodes);
+
+        var configValues = new List<TaxConfigValue>()
+        {
+            new TaxConfigValue{ Item = "FlatRate", Value = (decimal)17.5},
+            new TaxConfigValue{ Item = "FlatValue", Value = 10000},
+            new TaxConfigValue{ Item = "FlatValueRate", Value = 5},
+            new TaxConfigValue{ Item = "MinimumAnnualIncome", Value = 200000}
+        };
+
+        context.TaxConfigValue.AddRange(configValues);
+
+        var postalCode_7441 = context.TaxPostalCode.FirstOrDefault(o => o.Code.Equals("7441"));
+        var postalCode_A100 = context.TaxPostalCode.Where(o => o.Code.Equals("A100")).FirstOrDefault();
+        var postalCode_7000 = context.TaxPostalCode.Where(o => o.Code.Equals("7000")).FirstOrDefault();
+        var postalCode_1000 = context.TaxPostalCode.Where(o => o.Code.Equals("1000")).FirstOrDefault();
+
+        var taxRecords = new List<TaxRecord>()
+        {
+            new TaxRecord{ PostalCode = postalCode_7441, AnnualIncome = 500000, CalculatedTaxValue = 500},
+            new TaxRecord{ PostalCode = postalCode_A100, AnnualIncome = 400000, CalculatedTaxValue = 400},
+            new TaxRecord{ PostalCode = postalCode_7000, AnnualIncome = 300000, CalculatedTaxValue = 300},
+            new TaxRecord{ PostalCode = postalCode_1000, AnnualIncome = 200000, CalculatedTaxValue = 200},
+            new TaxRecord{ PostalCode = postalCode_7441, AnnualIncome = 10000, CalculatedTaxValue = 100},
+            new TaxRecord{ PostalCode = postalCode_1000, AnnualIncome = 55000, CalculatedTaxValue = 5500},
+            new TaxRecord{ PostalCode = postalCode_7441, AnnualIncome = 66000, CalculatedTaxValue = 6600},
+            new TaxRecord{ PostalCode = postalCode_7000, AnnualIncome = 79000, CalculatedTaxValue = 7900},
+            new TaxRecord{ PostalCode = postalCode_7441, AnnualIncome = 89000, CalculatedTaxValue = 890},
+            new TaxRecord{ PostalCode = postalCode_A100, AnnualIncome = 23400, CalculatedTaxValue = 2340},
+        };
+
+        context.TaxRecord.AddRange(taxRecords);
+
+        _ = context.SaveChanges();
+
+        return context;
+    }
+}
---
+++ TaxCalculationTest/Infrastucture.Tests/TaxRepositoryTest.cs
@@ -0,0 +1,138 @@
+﻿using Domain.Repository;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using TaxCalculationTest.Infrastucture.Tests.DBContextExtension;
+
+namespace TaxCalculationTest.Infrastucture.Tests;
+
+public class TaxRepositoryTest
+{
+    private ApplicationDBContext _context;
+
+    private ITaxConfigValueRepository _taxConfigValueRepository;
+    private ITaxProgressiveRatesRepository _taxProgressiveRatesRepository;
+    private ITaxPostalCodeRepository _taxPostalCodeRepository;
+    private ITaxRecordRepository _taxRecordRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+           .UseInMemoryDatabase($"TestTaxRecord-{Guid.NewGuid()}")
+           .Options;
+
+        _context = new ApplicationDBContext(options);
+
+        _ = _context.Database.EnsureDeleted();
+        _ = _context.Database.EnsureCreated();
+        _ = _context.AddTestData();
+
+        _taxPostalCodeRepository = new TaxPostalCodeRepository(_context);
+        _taxConfigValueRepository = new TaxConfigValueRepository(_context);
+        _taxProgressiveRatesRepository = new TaxProgressiveRatesRepository(_context);
+        _taxRecordRepository = new TaxRecordRepository(_context);
+    }
+
+    [Test]
+    public async Task TaxConfigValueRepository_GetCountOfTaxConfigValues()
+    {
+        //act
+        var result = await _taxConfigValueRepository.GetConfigValueAsync();
+
+        //assert
+        Assert.That(result.Count(), Is.EqualTo(4));
+    }
+
+    [Test]
+    public async Task TaxConfigValueRepository_GetTaxConfigValues()
+    {
+        //act
+        var result = await _taxConfigValueRepository.GetConfigValueAsync();
+
+        //assert
+        Assert.That(result.Where(o => o.Item.Equals("FlatRate")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Item.Equals("FlatValue")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Item.Equals("FlatValueRate")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Item.Equals("MinimumAnnualIncome")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Item.Equals("Invalid")).ToList().Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task TaxProgressiveRatesRepository_GetProgressiveRates()
+    {
+        //act
+        var output = await _taxProgressiveRatesRepository.GetProgressiveTaxMatrixAsync();
+        var result = output.FirstOrDefault(o => o.Rate.Equals(10));
+
+        //assert
+        Assert.That(result.Rate, Is.EqualTo(10));
+        Assert.That(result.FromValue, Is.EqualTo(0));
+        Assert.That(result.ToValue, Is.EqualTo(8350));
+    }
+
+    [Test]
+    public async Task TaxProgressiveRatesRepository_GetProgressiveRates_InvalidRate()
+    {
+        //act
+        var output = await _taxProgressiveRatesRepository.GetProgressiveTaxMatrixAsync();
+        var result = output.FirstOrDefault(o => o.Rate.Equals(0));
+
+        //assert
+        Assert.That(result, Is.EqualTo(null));
+    }
+
+    [Test]
+    public async Task TaxPostalCodeRepository_GetPostalCodes()
+    {
+        //act
+        var result = await _taxPostalCodeRepository.GetPostalCodeAsync();
+
+        //assert
+        Assert.That(result.Count(), Is.EqualTo(4));
+        Assert.That(result.Where(o => o.Code.Equals("7441")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Code.Equals("A100")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Code.Equals("7000")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Code.Equals("1000")).ToList().Count(), Is.EqualTo(1));
+        Assert.That(result.Where(o => o.Code.Equals("Invalid")).ToList().Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task TaxPostalCodeRepository_GetPostalCodeByCode()
+    {
+        //act
+        var result = await _taxPostalCodeRepository.GetPostalCodeByCodeAsync("1000");
+
+        //assert
+        Assert.That(result.Code, Is.EqualTo("1000"));
+        Assert.That(result.TaxCalculationType, Is.EqualTo(TaxType.Progressive));
+    }
+
+    [Test]
+    public async Task TaxRecordRepository_GetRecords_ShouldReturnFiveRecords()
+    {
+        //act
+        var result = await _taxRecordRepository.GetTaxRecordAsync();
+
+        //assert
+        Assert.That(result.Count(), Is.EqualTo(5));
+    }
+
+    [Test]
+    public async Task TaxRecordRepository_AddRecords()
+    {
+        //arrange
+        var taxRecord = new TaxRecord { PostalCode = new TaxPostalCode { Code = "A100", TaxCalculationType = TaxType.FlatValue }, AnnualIncome = 40, CalculatedTaxValue = 4 };
+
+        //act
+        _taxRecordRepository.Add(taxRecord);
+        await _context.SaveChangesAsync();
+
+        var result = await _taxRecordRepository.GetTaxRecordAsync(taxRecord.Id);
+
+        //assert
+        Assert.That(result.PostalCode, Is.EqualTo(taxRecord.PostalCode));
+        Assert.That(result.AnnualIncome, Is.EqualTo(taxRecord.AnnualIncome));
+        Assert.That(result.CalculatedTaxValue, Is.EqualTo(taxRecord.CalculatedTaxValue));
+    }
+}
